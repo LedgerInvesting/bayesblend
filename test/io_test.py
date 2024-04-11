2293e8cb8e1ed92b1e540818f968ebc17b2cf125
Nathaniel-Haines <nathaniel.b.haines@gmail.com>
2024-04-11 12:57:42 -0400

    test(io): add test for different posterior array names

--- test/io_test.py
+++ test/io_test.py
@@ -6,6 +6,7 @@ from cmdstanpy import CmdStanModel
 from bayesblend.io import Draws
 
 STAN_FILE = "test/stan_files/bernoulli_ppc.stan"
+STAN_FILE_DIFF = "test/stan_files/bernoulli_ppc_diff_name.stan"
 STAN_FILE_3D = "test/stan_files/bernoulli_ppc_3d.stan"
 DATA_FILE = "test/stan_data/bernoulli_data.json"
 
@@ -13,6 +14,7 @@ with open(DATA_FILE, "r") as f:
     BERN_DATA = json.load(f)
 
 MODEL = CmdStanModel(stan_file=STAN_FILE)
+MODEL_DIFF = CmdStanModel(stan_file=STAN_FILE_DIFF)
 
 
 def test_draws_init():
@@ -25,6 +27,13 @@ def test_draws_init():
 def test_draws_from_cmdstanpy():
     fit = MODEL.sample(data=BERN_DATA, chains=4, parallel_chains=4, seed=1234)
     assert Draws.from_cmdstanpy(fit)
+
+
+def test_draws_from_cmdstanpy_name():
+    fit = MODEL_DIFF.sample(data=BERN_DATA, chains=4, parallel_chains=4, seed=1234)
+    assert Draws.from_cmdstanpy(
+        fit, log_lik_name="log_lik_test", post_pred_name="post_pred_test"
+    )
 
 
 def test_draws_from_cmdstanpy_3d_variables():
